20b222747fa6d798407f92be78d24295040f211d
Irfan Nurul Susilo <irfansusilo88@gmail.com>
2023-09-23 01:34:08 +0700

    Add serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder, Result};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Home {
+    status: bool,
+    message: String,
+    result: String,
+}
+
+#[get("/")]
+async fn index() -> Result<impl Responder> {
+    let obj = Home {
+        status: true,
+        message: "OK".to_string(),
+        result: "Welcome RUST API V1".to_string(),
+    };
+    Ok(web::Json(obj))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
